e051cfed092ce86224bee9d4bd7ab51c5951ae76
LaudenLaruto <laudenlaruto@hotmail.fr>
2015-06-02 14:01:03 +0200

    Commencement de interfaceSaisie

---
+++ src/Vue/InterfaceSaisie.java
@@ -0,0 +1,10 @@
+package Vue;
+
+import java.awt.GridBagLayout;
+
+import javax.swing.*;
+
+public class InterfaceSaisie extends JPanel{
+	GridBagLayout gridBagLayout = new GridBagLayout();
+	JLabel labelNom = new JLabel("Nom:");
+}
