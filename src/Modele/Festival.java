3f7e6513996b56f36e6938527a9945d2f93bbcf3
LaudenLaruto <laudenlaruto@hotmail.fr>
2015-06-02 10:51:02 +0200

    Commencement de festival déclaration des champs ainsi que la javaDoc

--- src/Modele/Festival.java
+++ src/Modele/Festival.java
@@ -1,5 +1,37 @@
 package Modele;
-
+/**
+ * Classe qui defini les Festivals avec un nom, un genre, un tableau de Date, un lieu et un tableau de places
+ * 
+ * @author Benjamin
+ *
+ */
 public class Festival {
+
+	private String chNom;
+	private String chGenre;
+	private Date chDate[];
+	private String chLieu;
+	private int Places[];
+	
+	/**
+	 * 
+	 * @param chNom
+	 * @param chGenre
+	 * @param chDate
+	 * @param chLieu
+	 * @param places
+	 */
+	public Festival(String chNom, String chGenre, Date[] chDate, String chLieu, int[] places) {
+		chNom = chNom;
+		chGenre = chGenre;
+		chDate = chDate;
+		chLieu = chLieu;
+		Places = places;
+	}
+	public boolean estReservable(int parJour, int parMois){
+		return false;
+		
+	}
+	
 
 }
