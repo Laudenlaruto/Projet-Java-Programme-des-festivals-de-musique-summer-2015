b7de2be25deac40af5013905761f2a8caa7283c6
Loïc Bonnet <bonnetloic.lb@gmail.com>
2015-06-04 17:22:26 +0200

    Petite modification tests

--- src/Test/testDate.java
+++ src/Test/testDate.java
@@ -15,40 +15,47 @@ public class testDate {
 
 	@Test
 	public void testIntervalleFestival() throws ExceptionDate {
-		// Test 1 : 1 Juillet
+		
+		//Test 1 : 1 Juillet
+		
 		Date date1 = new Date(1,7,2015);
 		Date date2 = new Date(1,7,2015);
 		Date[] dateTab = date1.intervalleFestival(date2);
 		Date[] dateTabTest ={date1};
 		assertTrue("Date Differente",dateTab[0].precede(dateTabTest[0])==0&& dateTab.length==dateTabTest.length);
 		
-		// Test 2 : 2 Juillet - 1 Juillet
+		//Test 2 : 2 Juillet - 1 Juillet
+		
 		date1 = new Date(2,7,2015);
 		date2 = new Date (1,7,2015);
 		dateTab = date1.intervalleFestival(date2);
 		assertTrue("Date Differente",dateTab == null);
 		
-		// Test 3 31 Juillet - 1 Août 
+		//Test 3 : 31 Juillet - 1 Août 
+		
 		date1 = new Date(31,7,2015);
 		date2 = new Date (1,8,2015);
 		dateTab = date1.intervalleFestival(date2);
 		Date[] dateTabTest2 ={date1,date2};
 		assertTrue("Date Differente",dateTab[0].precede(dateTabTest2[0])==0&&dateTab[1].precede(dateTabTest2[1])==0&&dateTab.length==dateTabTest2.length);
 		
-		//Test 4 1 Août - 31 Juillet
+		//Test 4 : 1 Août - 31 Juillet
+		
 		date1 = new Date(1,8,2015);
 		date2 = new Date (31,7,2015);
 		dateTab = date1.intervalleFestival(date2);
 		assertTrue("Date Differente",dateTab == null);
 		
-		//Test 5 1 Août - 2 Août 
+		//Test 5 : 1 Août - 2 Août 
+		
 		date1 = new Date(1,8,2015);
 		date2 = new Date (2,8,2015);
 		dateTab = date1.intervalleFestival(date2);
 		Date[] dateTabTest3 ={date1,date2};
 		assertTrue("Date Differente",dateTab[0].precede(dateTabTest3[0])==0&&dateTab[1].precede(dateTabTest3[1])==0&&dateTab.length==dateTabTest3.length);
 		
-		// Test 6 2 Août - 1 Août
+		//Test 6 : 2 Août - 1 Août
+		
 		date1 = new Date(2,8,2015);
 		date2 = new Date (1,8,2015);
 		dateTab = date1.intervalleFestival(date2);
--- src/Test/testFestival.java
+++ src/Test/testFestival.java
@@ -16,28 +16,37 @@ public class testFestival {
 
 	@Test
 	public void testToStringReservation() throws ExceptionDate {
-		//Test 1 
+		
+		//Test 1 : 1 Juillet
+		
 		Date date1 = new Date(1,7,2015);
 		Date date2 = new Date(1,7,2015);
 		Festival festival = new Festival("test", "test", date1, date2, "test", 50, "test", 50);
 		assertTrue("To string Faux",festival.toStringReservation().equals("le Festival test \nAura lieu à test \nLe 1 Juillet"));
-		//Test 2 
+		
+		//Test 2 : 1 Août
+		
 		date1 = new Date(1,8,2015);
 		date2 = new Date(1,8,2015);
 		festival = new Festival("test", "test", date1, date2, "test", 50, "test", 50);
 		assertTrue("To string Faux",festival.toStringReservation().equals("le Festival test \nAura lieu à test \nLe 1 Août"));
-		//Test 3
+		
+		//Test 3 : 12 Juillet - 15 Août
+		
 		date1 = new Date(12,7,2015);
 		date2 = new Date(15,8,2015);
-		festival = new Festival("test", "test", date1, date2, "test", 50, "test", 50);
-		//System.out.println(festival.toStringReservation());
+		festival = new Festival("test", "test", date1, date2, "test", 50, "test", 50);		
 		assertTrue("To string Faux",festival.toStringReservation().equals("le Festival test \nAura lieu à \ntest \nDu 12 Juillet au 15 Août"));
-		//Test 4
+		
+		//Test 4 : 1 Juillet - 19 Juillet
+		
 		date1 = new Date(1,7,2015);
 		date2 = new Date(19,7,2015);
 		festival = new Festival("test", "test", date1, date2, "test", 50, "test", 50);
 		assertTrue("To string Faux",festival.toStringReservation().equals("le Festival test \nAura lieu à test \nDu 1 au 19 Juillet"));
-		//Test 5
+		
+		//Test 5 : 2 Août - 15 Août
+		
 		date1 = new Date(2,8,2015);
 		date2 = new Date(15,8,2015);
 		festival = new Festival("test", "test", date1, date2, "test", 50, "test", 50);
@@ -45,21 +54,29 @@ public class testFestival {
 	}
 	@Test
 	public void testnombreDePlaceTotal() throws ExceptionDate{
-		//Test 1
+		
+		//Test 1 : 2 Dates, 10 places par date
+		
 		Date date1 = new Date(1,7,2015);
 		Date date2 = new Date(2,7,2015);
 		Festival festival = new Festival("test", "test", date1, date2, "test", 10, "test", 50);
 		assertTrue("Nombre de place ne correspond pas",festival.nombreDePlaceTotal()==20);
-		//Test 2 
+		
+		//Test 2 : 2 Dates, 0 places par date
+		
 		date1 = new Date(1,7,2015);
 		date2 = new Date(2,7,2015);
 		festival = new Festival("test", "test", date1, date2, "test", 0, "test", 50);
 		assertTrue("Nombre de place ne correspond pas",festival.nombreDePlaceTotal()==0);
-		//Test 3
+		
+		//Test 3 : 1 Date, 10 places par date
+		
 		date1 = new Date(1,7,2015);
 		festival = new Festival("test", "test", date1, date1, "test", 10, "test", 50);
 		assertTrue("Nombre de place ne correspond pas",festival.nombreDePlaceTotal()==10);
-		//Test 4
+		
+		//Test 4 : 1 Date, 0 places par date
+		
 		date1 = new Date(1,7,2015);
 		festival = new Festival("test", "test", date1, date1, "test", 0, "test", 50);
 		assertTrue("Nombre de place ne correspond pas",festival.nombreDePlaceTotal()==0);
